34f0ec40eade61efbed994ecb777d641623cc5c9
bberdnikov <b.berdnikov@icloud.com>
2022-06-21 17:29:21 +0600

    chore: add element ui

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,6 +7,8 @@
 require('./bootstrap');
 
 window.Vue = require('vue').default;
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 
 /**
  * The following block of code may be used to automatically register your
@@ -19,7 +21,7 @@ window.Vue = require('vue').default;
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+Vue.component('quiz-component', require('./components/QuizComponent.vue').default);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
@@ -27,6 +29,8 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
+Vue.use(ElementUI);
+
 const app = new Vue({
-    el: '#app',
+  el: '#app',
 });
